00d6aec14e4d61b18094f4ae6c6f8d9347950647
script kiddie <anish.chittu2007@gmail.com>
2024-12-08 02:05:07 +0100

    made extraction work

--- png/multiDepthRGB.go
+++ png/multiDepthRGB.go
@@ -26,14 +26,6 @@ import (
 // havnt tested it yet but if it does then omfg
 
 // teste stuff remoe in prod
-var test = []RgbChannel{{255, 255, 255},{255, 255, 255},{255, 255, 255},{255, 255, 255},{255, 255, 255},{255, 255, 255},{255, 255, 255},{255, 255, 255},{255, 255, 255},{255, 255, 255},{255, 255, 255},}
-
-func printit(ch  []RgbChannel) {
-	for _, v := range ch {
-		fmt.Printf("{%08b, %08b, %08b\n}", v.R, v.G, v.B)
-	}
-}
-
 func EmbedIntoRGBchannelsWithDepth(RGBchannels []RgbChannel, data []byte, depth uint8) ([]RgbChannel, error) {
 	if depth > 7 {
 		return nil, fmt.Errorf("bit depth exeeds 7")
@@ -83,34 +75,36 @@ func ExtractDataFromRGBchannelsWithDepth(RGBchannels []RgbChannel, depth uint8) 
 	bitCount := 0
 
 	for i := 0; i < len(RGBchannels); i++ {
-		r := s.GetBit(RGBchannels[i].R, 0)
-		currentByte = (currentByte << 1) | (r & 1)
-		bitCount++
+		for bd := depth+1; bd > 0; bd-- {
+			r := s.GetBit(RGBchannels[i].R, bd-1)
+			currentByte = (currentByte << 1) | (r & 1)
+			bitCount++
 
-		if bitCount == 8 {
-			byteSlice = append(byteSlice, currentByte)
-			currentByte = 0
-			bitCount = 0
-		}
+			if bitCount == 8 {
+				byteSlice = append(byteSlice, currentByte)
+				currentByte = 0
+				bitCount = 0
+			}
 
-		g := s.GetBit(RGBchannels[i].G, 0)
-		currentByte = (currentByte << 1) | (g & 1)
-		bitCount++
+			g := s.GetBit(RGBchannels[i].G, bd-1)
+			currentByte = (currentByte << 1) | (g & 1)
+			bitCount++
 
-		if bitCount == 8 {
-			byteSlice = append(byteSlice, currentByte)
-			currentByte = 0
-			bitCount = 0
-		}
+			if bitCount == 8 {
+				byteSlice = append(byteSlice, currentByte)
+				currentByte = 0
+				bitCount = 0
+			}
 
-		b := s.GetBit(RGBchannels[i].B, 0)
-		currentByte = (currentByte << 1) | (b & 1)
-		bitCount++
+			b := s.GetBit(RGBchannels[i].B, bd-1)
+			currentByte = (currentByte << 1) | (b & 1)
+			bitCount++
 
-		if bitCount == 8 {
-			byteSlice = append(byteSlice, currentByte)
-			currentByte = 0
-			bitCount = 0
+			if bitCount == 8 {
+				byteSlice = append(byteSlice, currentByte)
+				currentByte = 0
+				bitCount = 0
+			}
 		}
 	}
 
