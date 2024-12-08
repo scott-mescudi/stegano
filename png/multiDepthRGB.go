5e5014eeb984f762343062c2161110f421606216
script kiddie <anish.chittu2007@gmail.com>
2024-12-08 01:47:14 +0100

    OMG ITS WORLS for embedding

--- png/multiDepthRGB.go
+++ png/multiDepthRGB.go
@@ -6,30 +6,71 @@ import (
 	s "github.com/scott-mescudi/stegano/utils"
 )
 
+// how the fuck am i going to do this?????
+// maybe try double loop since data is already spilt in to 3 just iterate ofer groups and embed them based on the depths
 
+// so with depth of 2(3 without 0 index) it would iterate over groups three times
+// first iteration would embed at bit index 2 
+// second iteration would embed on bitindex 1
+// third iteratioon would embed on index 0
+
+// but how will i do this?
+// kms?
+
+//same process for ewxtarction
+
+// bro  there  is no fucking way to test tjhos
+// aaaaaaaaaaahhdLSHREDDLJVFHAELHJFBVGLJHEFBVLJHWBHEFBVHGJEDFWKBGHJVBGJHDASWFVJGUHCDWUGVYFEVCITYGHbbbb
+
+// bok this might work
+// havnt tested it yet but if it does then omfg
+
+// teste stuff remoe in prod
+var test = []RgbChannel{{255, 255, 255},{255, 255, 255},{255, 255, 255},{255, 255, 255},{255, 255, 255},{255, 255, 255},{255, 255, 255},{255, 255, 255},{255, 255, 255},{255, 255, 255},{255, 255, 255},}
+
+func printit(ch  []RgbChannel) {
+	for _, v := range ch {
+		fmt.Printf("{%08b, %08b, %08b\n}", v.R, v.G, v.B)
+	}
+}
 
 func EmbedIntoRGBchannelsWithDepth(RGBchannels []RgbChannel, data []byte, depth uint8) ([]RgbChannel, error) {
 	if depth > 7 {
 		return nil, fmt.Errorf("bit depth exeeds 7")
 	}
 
+	if (len(data)*8)+32 > len(RGBchannels) * 3 * int(depth) {
+		return nil, fmt.Errorf("data is too big")
+	}
+
+	
 	lenOfDataInBinary := s.Int32ToBinary(int32(len(data)))
 	binaryData := s.BytesToBinary(data)
 	combinedData := append(lenOfDataInBinary, binaryData...)
 
 	z := splitIntoGroupsOfThree(combinedData)
+	
+	curbit := depth
+	index := 0
 
-	for i := 0; i < len(z); i+=int(depth)+1 {
-		if z[i].r != s.GetBit(RGBchannels[i].R, 0) {
-			RGBchannels[i].R = s.FlipBit(RGBchannels[i].R, 0)
+	for i := 0; i < len(z); i++ {
+		if z[i].r != s.GetBit(RGBchannels[index].R, curbit) {
+			RGBchannels[index].R = s.FlipBit(RGBchannels[index].R, curbit)
 		}
 
-		if z[i].g != s.GetBit(RGBchannels[i].G, 0) {
-			RGBchannels[i].G = s.FlipBit(RGBchannels[i].G, 0)
+		if z[i].g != s.GetBit(RGBchannels[index].G, curbit) {
+			RGBchannels[index].G = s.FlipBit(RGBchannels[index].G, curbit)
 		}
 
-		if z[i].b != s.GetBit(RGBchannels[i].B, 0) {
-			RGBchannels[i].B = s.FlipBit(RGBchannels[i].B, 0)
+		if z[i].b != s.GetBit(RGBchannels[index].B, curbit) {
+			RGBchannels[index].B = s.FlipBit(RGBchannels[index].B, curbit)
+		}
+
+		if curbit != 0 {
+			curbit--
+		} else {
+			curbit = depth
+			index++
 		}
 	}
 
