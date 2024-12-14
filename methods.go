f65c7faf9bc6a5d41b401201d3c1aa7d90499c59
script kiddie <anish.chittu2007@gmail.com>
2024-12-14 21:41:52 +0100

    added methods

--- methods.go
+++ methods.go
@@ -18,7 +18,7 @@ type secureExtractHandler struct {
 	concurrency int
 }
 
-// NewembedHandler initializes an embedHandler
+// NewEmbedHandlerWithConcurrency initializes an embedHandler with a specified concurrency
 func NewEmbedHandlerWithConcurrency(concurrency int) (*embedHandler, error) {
 	if concurrency <= 0 {
 		return nil, fmt.Errorf("invalid number of goroutines")
@@ -27,13 +27,12 @@ func NewEmbedHandlerWithConcurrency(concurrency int) (*embedHandler, error) {
 	return &embedHandler{concurrency: concurrency}, nil
 }
 
-// NewembedHandler initializes an embedHandler
+// NewEmbedHandler initializes an embedHandler with default concurrency
 func NewEmbedHandler() (*embedHandler) {
 	return &embedHandler{concurrency: 1}
 }
 
-
-// NewextractHandler initializes an extractHandler
+// NewExtractHandlerWithConcurrency initializes an extractHandler with a specified concurrency
 func NewExtractHandlerWithConcurrency(concurrency int) (*extractHandler, error) {
 	if concurrency <= 0 {
 		return nil, fmt.Errorf("invalid number of goroutines")
@@ -42,7 +41,35 @@ func NewExtractHandlerWithConcurrency(concurrency int) (*extractHandler, error) 
 	return &extractHandler{concurrency: concurrency}, nil
 }
 
-// NewextractHandler initializes an extractHandler
+// NewExtractHandler initializes an extractHandler with default concurrency
 func NewExtractHandler() (*extractHandler) {
 	return &extractHandler{concurrency: 1}
+}
+
+// NewSecureEmbedHandlerWithConcurrency initializes a secureEmbedHandler with a specified concurrency
+func NewSecureEmbedHandlerWithConcurrency(concurrency int) (*secureEmbedHandler, error) {
+	if concurrency <= 0 {
+		return nil, fmt.Errorf("invalid number of goroutines")
+	}
+
+	return &secureEmbedHandler{concurrency: concurrency}, nil
+}
+
+// NewSecureEmbedHandler initializes a secureEmbedHandler with default concurrency
+func NewSecureEmbedHandler() (*secureEmbedHandler) {
+	return &secureEmbedHandler{concurrency: 1}
+}
+
+// NewSecureExtractHandlerWithConcurrency initializes a secureExtractHandler with a specified concurrency
+func NewSecureExtractHandlerWithConcurrency(concurrency int) (*secureExtractHandler, error) {
+	if concurrency <= 0 {
+		return nil, fmt.Errorf("invalid number of goroutines")
+	}
+
+	return &secureExtractHandler{concurrency: concurrency}, nil
+}
+
+// NewSecureExtractHandler initializes a secureExtractHandler with default concurrency
+func NewSecureExtractHandler() (*secureExtractHandler) {
+	return &secureExtractHandler{concurrency: 1}
 }
