a3480887c77f74a539b340c4006537793b6387d1
script kiddie <anish.chittu2007@gmail.com>
2025-01-04 19:41:30 +0100

    better comments

--- variables.go
+++ variables.go
@@ -11,7 +11,7 @@ const (
 	MaxBitDepth uint8 = 7
 )
 
-// Errors for image_embedder.go
+// Errors for image_embedder.go and image_core.go
 var (
     ErrDepthOutOfRange      = errors.New("bitDepth is out of range (0-7)")
     ErrFailedToExtractRGB   = errors.New("failed to extract RGB channels from the image")
